8dd3846086877e6ff2c4d7bd193efa331f25d15c
Lucas Nseyep <lucas.nseyep@gmail.com>
2023-06-29 13:08:26 +0100

    build: new and create actions for climes

--- app/controllers/climes_controller.rb
+++ app/controllers/climes_controller.rb
@@ -1,5 +1,22 @@
 class ClimesController < ApplicationController
   def index
-    
+  end
+
+  def new
+    @clime = Clime.new
+  end
+
+  def create
+    @clime = Clime.new(clime_params)
+  end
+
+  private
+
+  def clime_params
+    params.require(:clime).permit(:latitude, :longitude)
+  end
+
+  def get_clime_data
+    #api call
   end
 end
--- app/javascript/controllers/get_coordinates_controller.js
+++ app/javascript/controllers/get_coordinates_controller.js
@@ -2,10 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="get-coordinates"
 export default class extends Controller {
+  static targets = ["latitude", "longitude"]
+
   connect() {
+    console.log("Hello from get coordinates controller!")
+  }
+
+  get(){
     navigator.geolocation.getCurrentPosition((data) => {
       console.log(data)
-      console.log(data.coords.latitude)
     })
   }
 }
